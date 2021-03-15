b169e3ee8235cee7cf1d6749439cc421ec689fe4
teodora8513 <jevtic.teodora@gmail.com>
2021-03-16 00:06:16 +0100

    Dokumentovan interfejs BibliotekaInterfejs

--- src/biblioteka/inrterfejs/BibliotekaInterface.java
+++ src/biblioteka/inrterfejs/BibliotekaInterface.java
@@ -6,10 +6,30 @@ import biblioteka.Autor;
 import biblioteka.Knjiga;
 
 public interface BibliotekaInterface {
-
+	/**
+	 * Interfejs koji predstavlja biblioteku
+	 * 
+	 * @author Teodora
+	 * 
+	 *         Dodaje novu knjigu u biblioteku
+	 * @param knjiga
+	 *            koja se dodaje
+	 * @throws java.lang.NullPointerException
+	 *             ako je uneta knjiga null
+	 * @throws java.lang.RuntimeException
+	 *             ako je uneta knjiga vec postoji u biblioteci
+	 */
 	public void dodajKnjiga(Knjiga knjiga);
+
+	/**
+	 * Brise knjigu
+	 * 
+	 * @param knjiga
+	 *            koja se brise
+	 */
 	public void obrisiKnjigu(Knjiga knjiga);
+
 	public LinkedList<Knjiga> vratiSveKnjige();
-	public LinkedList<Knjiga> pronadjiKnjigu(
-			Autor autor, String isbn, String naslov, String izdavac);
+
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac);
 }
